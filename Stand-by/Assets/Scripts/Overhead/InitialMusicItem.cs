b9a0362fd633edcf9c2e6bf83f3126e717121113
Zya Gurau <92696912+Zya-Gurau@users.noreply.github.com>
2024-09-17 17:52:23 +1200

    refactor - changed UI sizes

--- Stand-by/Assets/Scripts/Overhead/InitialMusicItem.cs
+++ Stand-by/Assets/Scripts/Overhead/InitialMusicItem.cs
@@ -14,8 +14,16 @@ public class InitialMusicItem : MonoBehaviour
         item.genre = "Pop";
         item.format = "CD";
         item.sprite = sprite;
+
+        MusicItem item2 = new MusicItem();
+        item2.musicName = "Rock n Roll Disc";
+        item2.genre = "Rock";
+        item2.format = "CD";
+        item2.sprite = sprite;
+
         inventory = GameObject.Find("Player").GetComponent<MusicInventoryManager>();
         inventory.AddItem(item);
+        inventory.AddItem(item2);
     }
 
     // Update is called once per frame
